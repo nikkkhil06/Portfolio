30abe7b6bde42e9ffe7655fca9574c547643f726
nikkkhil06 <nikhilvishwakarma0351@gmail.com>
2026-06-14 12:14:11 +0530

    Polishing of portfolio DONE

---
+++ home/home.js
@@ -0,0 +1,21 @@
+const items = document.querySelectorAll(".nav-item");
+        const slider = document.querySelector(".mainslider");
+        const firstItem = items[0];
+        firstItem.classList.add("active");
+        slider.style.left = firstItem.offsetLeft + "px";
+        slider.style.width = firstItem.offsetWidth + "px";
+        items.forEach(item=>{
+            item.addEventListener("click",(e)=>{
+                e.preventDefault();
+                items.forEach(i=>{
+                    i.classList.remove("active"); 
+            });
+                item.classList.add("active");
+                slider.style.left = item.offsetLeft + "px";
+                slider.style.width = item.offsetWidth + "px";
+                const section=document.querySelector(item.getAttribute("href"));
+                window.scrollTo({
+                    top: section.offsetTop-80,
+                    behavior:"smooth"
+                });
+        });  });
